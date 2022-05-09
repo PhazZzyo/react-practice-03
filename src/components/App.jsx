bbf06e47f3f1563a58422794af951df030578572
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-05-09 03:00:35 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Form from './Form/Form';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  render() {
+    /////const
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <Form></Form>
+
+        <h2>Contacts</h2>
+      </>
+    );
+  }
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+
+export class Form extends Component {
+  state = {
+    name: '',
+  };
+
+  formInputId = shortid.generate();
+
+  handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor={this.formInputId}>
+          Name
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            id={this.formInputId}
+          />
+        </label>
+        <button type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
