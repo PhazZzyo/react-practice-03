f961c29778ac2829746c0a73cd99b0e7bdc330dd
Alex <41620816+PhazZzyo@users.noreply.github.com>
2022-05-12 17:44:03 +0300

    complete
    task complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,21 @@ export class App extends Component {
     ...INITIAL_STATE,
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = data => {
     const { name, number } = data;
     const { contacts } = this.state;
